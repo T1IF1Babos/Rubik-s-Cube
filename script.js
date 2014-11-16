94bc178350d202852d21ffa9d8e71062b36e7ddf
RedCrafter1 <hendeljo@yahoo.de>
2014-11-16 21:10:38 +0100

    - add script files (lib/stats.js, loaders/OBJLoader.js) - add div Container for stats - add function drawCube

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 "use strict";
 
 var canvas;
-
 var scene, camera, renderer;
+
+var stats;
+
+var animation = true;
+
+var cube;
 
 function init() {
 	// Set Variables
@@ -16,6 +21,7 @@ function init() {
 
 	renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 	renderer.setSize(canvas.width, canvas.height);
+	renderer.setClearColor("#dddddd");
 	document.body.appendChild(renderer.domElement);
 
 	scene = new THREE.Scene();
@@ -26,6 +32,12 @@ function init() {
 	camera.position.z = 5;
 	camera.lookAt(scene.position);
 
+	// set State
+	stats = new Stats();
+	document.getElementById("stats").appendChild(stats.domElement);
+
+
+	// Set Light
 	var ambient = new THREE.AmbientLight("#222222");
 	var directionalLight1 = new THREE.DirectionalLight("#ffffff");
 	var directionalLight2 = new THREE.DirectionalLight("#ffffff");
@@ -36,6 +48,40 @@ function init() {
 	scene.add(ambient);
 	scene.add(directionalLight1);
 	scene.add(directionalLight2);
+
+
+	drawCube();
+	render();
+}
+function drawCube() {
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function (item, loaded, total) {
+		console.log(item, loaded, total);
+	};
+
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+
+	var onError = function ( xhr ) {
+	};
+
+	var loader = new THREE.OBJLoader(manager);
+	loader.load("objects/cubeTexture.obj", function(object) {
+		object.position.set(0, 0, 0);
+		scene.add(object);
+	}, onProgress, onError);
+}
+function render() {
+	if (animation) requestAnimationFrame(render);
+
+
+
+	stats.update();
+	renderer.render(scene, camera);
 }
 
 window.addEventListener("load", init);
